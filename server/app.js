252060f2204452a27a6687b702c35097c80f3f4b
Thraa Muhammed <thraa.muh@gmail.com>
2023-06-26 13:27:57 +0300

    home & port

--- server/app.js
+++ server/app.js
@@ -22,7 +22,7 @@ import { clientsRouter } from './clients/clientsRoutes';
 
 
 const debug = makeDebug('app:main');
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 'https://integrate-daq.com/';
 
 function initializeServerEnvironment() {
   dotenv.config();
